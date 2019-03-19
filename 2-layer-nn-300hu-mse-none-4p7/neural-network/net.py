089f6b3cfe7cacd7a4befe27c428a8cae7e9a460
Gautam <gramasu000@citymail.cuny.edu>
2019-03-19 12:41:10 -0400

    Coded gradient descent for 2-layer-network

--- 2-layer-nn-300hu-mse-none-4p7/neural-network/net.py
+++ 2-layer-nn-300hu-mse-none-4p7/neural-network/net.py
@@ -10,42 +10,60 @@ class NeuralNetwork:
     def set_random_weights(self):
         self.weights = {
             "w1": np.random.randn((self.input_size, self.hidden_size)),
+            "b1": np.random.randn((1, self.hidden_size)),
             "w2": np.random.randn((self.hidden_size, self.output_size)),
-            "b1": np.random.randn((1, self.hidden_size))
             "b2": np.random.randn((1, self.output_size))
         }
     
     def set_specific_weights(self, weights):
         self.weights = weights
 
-    @staticmethod
     def learning_rate(iter):
         return 0.001
 
-    @staticmethod
-    def sofm(x):
-        s = np.exp(x) 
-        return s / np.sum(s)
-    
-    @staticmethod
-    def relu(x):
-        return np.greater(x, 0) * x
-
-    def _forward_prop(self, input):
+    def _forward_prop(self, input):  
         h1 = np.matmul(input, self.weights["w1"]) + self.weights["b1"]
-        r1 = self.relu(h1)
+        a1 = np.greater(h1, 0) * h1
         h2 = np.matmul(r1, self.weights["w2"]) + self.weights["b2"]
-        probs = self.sofm(h2)
-        return h1, r1, h2, probs
+        a2 = np.exp(h2)
+        a2 /= np.sum(a2)
+        self.fprop = {
+            "in": input
+            "h1": h1,
+            "a1": a1,
+            "h2": h2,
+            "a2": a2 
+        }
     
     def predict(self, input):
-        _,_,_,probs = self._forward_prop(input)
-        preds = np.argmax(probs, axis=1)
+        self._forward_prop(input)
+        preds = np.argmax(self.fprop["a2"], axis=1)
         return preds
 
     def _compute_loss(self, probs, labels):
         loss = np.sum(-np.log(probs) * labels)
         return loss
 
-    def _compute_gradients(self, h1, r1, h2, probs):
-        # TODO: Finish this
+    def _compute_gradients(self, labels):
+        dh2 = self.fprop["a2"] - labels
+        dw2 = np.matmul(self.fprop["a1"].T, dh2)
+        db2 = dh2 
+        da1 = np.matmul(dh2, self.weights["w2"].T)
+        dh1 = da1 * np.greater(self.fprop["h1"], 0)
+        dw1 = np.matmul(self.fprop["in"].T, dh1)
+        db1 = dh1 
+        self.bprop = {
+            "dw2": dw2,
+            "db2": db2,
+            "dw1": dw1,
+            "db1": db1
+        }
+
+    def _evaluate_gradients(self, iter):
+        for key in self.weights:
+            bkey = "d" + key
+            self.weights[key] -= self.learning_rate(iter)*self.bprop[bkey]
+ 
+    def _flush(self):
+        self.fprop = {}
+        self.bprop = {}
