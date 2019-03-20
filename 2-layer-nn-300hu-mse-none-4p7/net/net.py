690ee728e8ab79d2b490002c22a51358da17a3ba
Gautam <gramasu000@citymail.cuny.edu>
2019-03-20 10:17:09 -0400

    Coded train method, combined compute_loss into forward propogate, changed iter to epoch

--- 2-layer-nn-300hu-mse-none-4p7/net/net.py
+++ 2-layer-nn-300hu-mse-none-4p7/net/net.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class NeuralNetwork:
+class FourPointSevenNet:
 
     def __init__(self):
         self.input_size = 784
@@ -18,39 +18,36 @@ class NeuralNetwork:
     def set_specific_weights(self, weights):
         self.weights = weights
 
-    def learning_rate(iter):
+    def learning_rate(epoch):
         return 0.001
 
-    def _forward_prop(self, input):  
-        h1 = np.matmul(input, self.weights["w1"]) + self.weights["b1"]
+    def _forward_prop(self, inputs, labels):  
+        h1 = np.matmul(inputs, self.weights["w1"]) + self.weights["b1"]
         a1 = np.greater(h1, 0) * h1
         h2 = np.matmul(r1, self.weights["w2"]) + self.weights["b2"]
         a2 = np.exp(h2)
         a2 /= np.sum(a2)
+        loss = np.sum(-np.log(a2) * labels)
         self.fprop = {
-            "in": input
             "h1": h1,
             "a1": a1,
             "h2": h2,
-            "a2": a2 
+            "a2": a2,
+            "loss": loss
         }
     
-    def predict(self, input):
-        self._forward_prop(input)
+    def predict(self, inputs):
+        self._forward_prop(inputs)
         preds = np.argmax(self.fprop["a2"], axis=1)
         return preds
 
-    def _compute_loss(self, probs, labels):
-        loss = np.sum(-np.log(probs) * labels)
-        return loss
-
-    def _compute_gradients(self, labels):
+    def _compute_gradients(self, inputs, labels):
         dh2 = self.fprop["a2"] - labels
         dw2 = np.matmul(self.fprop["a1"].T, dh2)
         db2 = dh2 
         da1 = np.matmul(dh2, self.weights["w2"].T)
         dh1 = da1 * np.greater(self.fprop["h1"], 0)
-        dw1 = np.matmul(self.fprop["in"].T, dh1)
+        dw1 = np.matmul(inputs.T, dh1)
         db1 = dh1 
         self.bprop = {
             "dw2": dw2,
@@ -59,11 +56,12 @@ class NeuralNetwork:
             "db1": db1
         }
 
-    def _evaluate_gradients(self, iter):
+    def _eval_grad_descent(self, epoch):
         for key in self.weights:
             bkey = "d" + key
-            self.weights[key] -= self.learning_rate(iter)*self.bprop[bkey]
+            self.weights[key] -= self.learning_rate(epoch)*self.bprop[bkey]
  
-    def _flush(self):
-        self.fprop = {}
-        self.bprop = {}
+    def train(self, epoch, inputs, labels):
+        self._forward_prop(inputs, labels)
+        self._compute_gradients(inputs, labels)
+        self._eval_grad_descent(epoch)
