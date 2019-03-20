7ab96d07cbe98dea963e1f88baab71c6ca2804f4
Gautam <gramasu000@citymail.cuny.edu>
2019-03-20 13:01:54 -0400

    Created MnistExtractor class to extract mnist data in batches

--- 2-layer-nn-300hu-mse-none-4p7/preprocess/mnist_extract.py
+++ 2-layer-nn-300hu-mse-none-4p7/preprocess/mnist_extract.py
@@ -1,34 +1,38 @@
 import os
 import numpy as np
 
-from mnist_check import check_file 
+from mnist_check import _MNIST_SIZES, _MNIST_NUM_EXAMPLES, 
+                        _MNIST_DIMENSIONS, _MNIST_OFFSETS
 
-def _extract_images(f):
-    f.seek(4, os.SEEK_SET)
-    num_examples = int.from_bytes(f.read(4))
-    num_rows = int.from_bytes(f.read(4))
-    num_cols = int.from_bytes(f.read(4))
-    num_feat = num_rows * num_cols
-    np_array = np.empty((num_examples, num_feat))    
-    for i in num_examples:
-        for j in num_feat:
-            pxl_val = int.from_bytes(f.read(1), byteorder="big", signed=False)
-            np_array[i,j] = float(pxl_val)
-    return np_array
+class MnistExtractor:
 
-def _extract_labels(f):
-    f.seek(4, os.SEEK_SET)
-    num_examples = int.from_bytes(f.read(4))
-    np_array = np.zeros((num_examples, 10))    
-    for i in num_examples:
-        label_int = int.from_bytes(f.read(1), byteorder="big", signed=False)
-        np_array[i, label_int] = 1
-    return np_array
+    def __init__(self, image_filepath, label_filepath):
+        self.fimage = open(image_filepath, "rb")
+        self.flabel = open(label_filepath, "rb")
 
-def extract(file_path):
-    with open(file_path, "rb") as f:
-        use, type = check_file(f)
-        if type is "image":
-            return _extract_images(f)
-        if type is "label":
-            return _extract_labels(f)
+    def _extract_image_batch(self, start, end):
+        np_array = np.empty((end-start, _MNIST_DIMENSIONS["image"]))
+        self.fimage.seek(_MNIST_OFFSETS["image"] + start*_MNIST_DIMENSION["image"])
+        for i in range(end-start):
+            for j in range(num_feat):
+                pxl_val = int.from_bytes(self.fimage.read(1), 
+                                    byteorder="big", signed=False)
+                np_array[i,j] = float(pxl_val)
+        return np_array
+                 
+    def _extract_label_batch(self, start, end):
+        np_array = np.zeros((end-start, _MNIST_DIMENSIONS["label"]))
+        self.flabel.seek(_MNIST_OFFSETS["label"] + start)
+        for i in range(end-start):
+            label_int = int.from_bytes(self.flabel.read(1), 
+                                    byteorder="big", signed=False)
+            np_array[i, label_int] = 1
+        return np_array
+
+    def extract_batch(self, start, end):
+        return _extract_image_batch(self, start, end),
+                _extract_label_batch(self, start, end)
+
+    def __del__(self):
+        self.fi.close()
+        self.fl.close()
